b26afed9b547d5bc47621bb041fa2f7a57a56fa6
Sander Daskov <luxeon@live.com>
2022-05-15 18:20:36 +0300

    Refractored code dublicate with Promice for data

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch');
 
-app.set('view egnine','ejs')
-app.set('views',path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/',function (req,res){
-    res.render('index.ejs')
+const key = '25df10e0f35664b5c6c1b7b1018597ca';
+
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(responce => {
+            return responce.json()
+        })
+            .then(data =>{
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+                let result = {
+                    description: description,
+                    city: city,
+                    temp: temp,
+                    error: null
+                }
+                resolve(result)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+app.all('/', function (req, res){
+    let city
+    if(req.method == 'GET'){
+        city = "Tartu"
+    }
+    if(req.method == 'POST'){
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data => {
+            res.render('index', data)
+        })
 })
-
 app.listen(3000)
